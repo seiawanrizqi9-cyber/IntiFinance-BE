f57bbb0eaf8230a4fdf77a3b0d2b0ae5d3eef165
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-22 10:50:47 +0700

    feat: create modules auth, categories, reports, transactions, users in src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
+import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
   imports: [
@@ -13,6 +18,16 @@ import { ConfigModule } from '@nestjs/config';
         uri: config.get<string>('MONGODB_URI'),
       }),
     }),
+
+    AuthModule,
+
+    UsersModule,
+
+    CategoriesModule,
+
+    TransactionsModule,
+
+    ReportsModule,
   ],
 })
 export class AppModule {}
